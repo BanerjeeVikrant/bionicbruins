a1d622be71e44ad456bdc23744763d18069f4bea
BanerjeeVikrant <banerjeevikrant2002@gmail.com>
2017-05-08 23:29:23 -0700

    d

---
+++ android/getuserinfo.php
@@ -0,0 +1,41 @@
+<?php
+
+$servername = "localhost";
+$username1 = "root";
+$password = "H@ll054321";
+$dbname = "bionicbruins";
+
+// Create connection
+$conn = new mysqli($servername, $username1, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$username = $_POST['username'];
+
+
+$query = $conn->query("SELECT * FROM members WHERE username='$username'");
+$row = $query->fetch_assoc();
+
+$yourfirstname = $row['first_name'];
+$yourlastname = $row['last_name'];
+$yourprofilepic = $row['profile_pic'];
+$yourid = $row['id'];
+
+echo '
+{
+    "userdata": [
+                {
+                    "id":'.$yourid.',
+                    "firstname": "'.$yourfirstname.'",
+                    "lastname": "'.$yourlastname.'",
+                    "userpic": "'.$yourprofilepic.'",
+                    "name": "'.$yourfirstname. " " .$yourlastname.'",
+                }  
+    ]
+}
+
+';
+
+?>
